66e4a2100b3d974b4c64f038e754987070d60816
Andres Manco <andresmanco@gmail.com>
2018-06-21 02:05:52 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,11 @@
 def my_select(collection)
   arr = []
-  collection.each do |c|
-  arr << yield(c)
-        
+  collection.each do |col|
+      if yield(col)
+        arr << col 
   end
   arr
 end
+
+arr.my_select do |a|
+  a.even
